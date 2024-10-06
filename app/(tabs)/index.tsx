98e20692f00d4a4dfc3134a20d052b2b5ee4fa96
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2024-10-06 22:05:59 +0530

    IOS init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,14 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect } from 'react';
 
 export default function HomeScreen() {
+
+  useEffect(() => {
+    
+  }, [])
+  
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
